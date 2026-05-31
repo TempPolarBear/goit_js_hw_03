73fc3277b344a17f3adc6525189949a4c14287bd
Valeriia Kuznetsova <93194138+TempPolarBear@users.noreply.github.com>
2026-05-31 18:42:20 +0300

    new

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const newArray = firstArray.concat(secondArray)
+    if (newArray.length > maxLength) {
+        return newArray.slice(0, maxLength)
+    }
+    return newArray
+}
